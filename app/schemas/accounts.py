f209ebd114bdf9673ce94349be53754efbc6e221
Benzhamin <afraimovbinyomin2004@gmail.com>
2025-06-03 18:48:38 +0500

    schemas

--- app/schemas/accounts.py
+++ app/schemas/accounts.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel
 
 
-
-
 class UserBase(BaseModel):
     username: str
     email: str
     first_name: str
     last_name: str
-
+    phone_number: str
+    adress: str 
 
 class UserRegister(UserBase):
     password: str
@@ -16,11 +15,63 @@ class UserRegister(UserBase):
 class UserLogin(UserRegister):
     pass
 
-class UserOut(UserBase):
+class User(UserBase):
     id: int
+
+    class Confing:
+        orm_mode = True
 
 class Token(BaseModel):
     access: str
     refresh: str
     token_type: str
     
+
+class CountryBase(BaseModel):
+    title: str
+    is_active: bool
+
+class Country(CountryBase):
+    id: int 
+
+    class Config:
+        orm_mode = True
+
+class CurrencyBase(BaseModel):
+    country_id: int
+    currency_rate: int
+
+class Currency(CurrencyBase):
+    id: int 
+
+    class Config:
+        orm_mode = True
+
+
+
+class CategoryBase(BaseModel):
+    title: str
+    description: str
+    image: str
+    is_active: bool
+
+class Category(CategoryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SubCategoryBase(UserBase):
+    category_id: int
+    title: str
+    description: str
+    iamge: str
+    is_active: bool
+
+class SubCategory(SubCategoryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+        
